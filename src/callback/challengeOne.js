9189ba04b2391b2c0620a70d74def215a088c32e
Christiano0407 <placebo0704@gmail.com>
2020-12-15 09:02:47 -0600

    callback-1

--- src/callback/challengeOne.js
+++ src/callback/challengeOne.js
@@ -4,9 +4,12 @@
 
 //abrir la url, obtener la petición(.open)
 // readyState === 4 (valor para decir que ha sido completado) / status 200 (salió bien-web).
+//Petición por medio de XML, para Callback.
 
 let XMLHttpRequest = require(`xmlhttprequest`).XMLHttpRequest;
-//Petición por medio de XML, para Callback.
+
+let API = `https://rickandmortyapi.com/api/character/`;
+
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.open(`GET`, url_api, true);
@@ -22,3 +25,17 @@ function fetchData(url_api, callback) {
     } 
     xhttp.send();
 }
+
+//Callback help: Ejecutar muchas peticiones.
+fetchData(API, function(error1, data1) {
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2){
+        if(error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3) {
+          if(error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    })
+})
