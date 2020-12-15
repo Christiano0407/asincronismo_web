20b7b42546e8a1800b58985a27f5576e932f3fed
Christiano0407 <placebo0704@gmail.com>
2020-12-15 07:18:19 -0600

    callback

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback){
+    return callback(num1, num2);
+}
+console.log(calc(4, 4, sum));
+
+// 
+function date(callback) {
+    console.log(new Date);
+    setTimeout(function() {
+      let date = new Date;
+      callback(date);
+    },2000)
+}
+
+function printDate(dateNow){
+    console.log(dateNow);
+}
+date(printDate);
