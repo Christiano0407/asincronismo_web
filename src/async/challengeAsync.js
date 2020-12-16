dcd1638ce20fc0d7b29c6e673f4728027ac29615
Christiano0407 <placebo0704@gmail.com>
2020-12-16 09:21:59 -0600

    challenge-asincronismo

--- src/async/challengeAsync.js
+++ src/async/challengeAsync.js
@@ -1,2 +1,20 @@
 const fetchData = require(`../utils/fetchData`);
 const API = `https://rickandmortyapi.com/api/character/`;
+
+const anotherFunction = async (url_api) => {
+try{
+const data = await fetchData(url_api);
+const character = await fetchData(`${API}${data.results[0].id}`);
+const origin = await fetchData(character.origin.url);
+
+console.log(data.info.count);
+console.log(character.name);
+console.log(origin.dimension);
+}catch(error){
+ console.error(error);
+}
+}
+
+console.log(`ONE`);
+anotherFunction(API);
+console.log(`FATALITY`);
